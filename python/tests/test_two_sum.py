4f3060a1be17768d75218bce6369886563d882f5
mrfor <truevladimir93Dev@yandex.ru>
2023-01-22 21:11:55 +0300

    done test_two_sum

---
+++ python/tests/test_two_sum.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from scripts import two_sum as ts
+
+
+class Test(TestCase):
+    def test_two_sum(self):
+        # self.assertEqual(sorted(ts.two_sum([1, 2, 3], 4)), [0, 2])
+        # self.assertEqual(sorted(ts.two_sum([1234, 5678, 9012], 14690)), [1, 2])
+        self.assertEqual(sorted(ts.two_sum([2, 2, 3], 4)), [0, 1])
+        self.assertEqual(sorted(ts.two_sum([3, 2, 2], 4)), [1, 2])
