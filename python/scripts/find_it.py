239f47d72c610d2b47a387721ee516d22ddba8fe
mrfor <truevladimir93Dev@yandex.ru>
2023-01-22 22:53:11 +0300

    complite find_it

--- python/scripts/find_it.py
+++ python/scripts/find_it.py
@@ -1,2 +1,9 @@
+# https://www.codewars.com/kata/54da5a58ea159efa38000836/train/python
+from collections import Counter as cnt
+
+
 def find_it(seq):
-    return None
+    dct = cnt(seq)
+    for dig, count in dct.items():
+        if count % 2 != 0:
+            return dig
