1ec8fdeedd5f5c8e775723c4842c59e41c2a99cb
mrfor <truevladimir93Dev@yandex.ru>
2023-01-22 22:29:55 +0300

    refact test_two_sum

--- python/scripts/two_sum.py
+++ python/scripts/two_sum.py
@@ -1,18 +1,18 @@
 # https://www.codewars.com/kata/52c31f8e6605bcc646000082/train/python
 
 def two_sum(numbers, target):
-        nums = list(numbers)
-        for num in numbers:
+    nums = list(numbers)
+    for num in numbers:
 
-            find = target - num
-            try:
-                x = list(nums).index(num)
+        find = target - num
+        try:
+            x = list(nums).index(num)
+            y = list(nums).index(find)
+            if x == y:
+                nums[y] = None
                 y = list(nums).index(find)
-                if x == y:
-                    nums[y] = None
-                    y = list(nums).index(find)
-                    return [x, y]
-                else:
-                    return [x, y]
-            except ValueError:
-                continue
+                return [x, y]
+            else:
+                return [x, y]
+        except ValueError:
+            continue
